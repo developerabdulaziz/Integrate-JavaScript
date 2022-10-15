01d1a377ec006698d3732f00b916daada7220ad8
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-15 21:44:59 +0600

    properties add

--- properties.js
+++ properties.js
@@ -1,6 +1,13 @@
-/* 
+/*
 document.getElementsByTagName
 document.getElementById
 document.getElementsByClassName
-
+document.querySelector
+document.querySelectorAll
 */
+
+// Dynamic style, getAttribute, setAttribute, innerText, innerHTML
+
+// Styling dom properties, add and remove css class using JS
+
+// NodeList, htmlcollection, parentNode, childnodes, createElement
